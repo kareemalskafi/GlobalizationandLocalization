7c99e3db0cb126567795e6f316d566b99a2805de
Kareem Al-Skafi <80727181+kareemalskafi@users.noreply.github.com>
2023-11-17 11:55:41 +0300

    Create LanguageController.cs

---
+++ GlobalizationandLocalization/Controllers/Api/LanguageController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace GlobalizationandLocalization.Controllers.Api
+{
+        [Route("api/[controller]")]
+        [ApiController]
+
+    public class LanguageController : ControllerBase
+    {
+        private readonly IStringLocalizer<LanguageController> _Localizer;
+
+        public LanguageController(IStringLocalizer<LanguageController> localizer)
+        {
+            _Localizer = localizer;
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_Localizer["welcome"].Value);
+        }
+    }
+}
