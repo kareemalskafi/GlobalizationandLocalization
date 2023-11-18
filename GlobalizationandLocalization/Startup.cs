c46e56f39fb96402266ee526a901de5ff7bc518f
Kareem Al-Skafi <80727181+kareemalskafi@users.noreply.github.com>
2023-11-18 14:55:53 +0300

    newItem

--- GlobalizationandLocalization/Startup.cs
+++ GlobalizationandLocalization/Startup.cs
@@ -29,11 +29,19 @@ namespace GlobalizationandLocalization
         public void ConfigureServices(IServiceCollection services)
         {
 
-            // Localization Services
-            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.AddLocalization(options => options.ResourcesPath = "SharedResource");
+
             services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
                 .AddDataAnnotationsLocalization();
 
+
+
+            /* Localization Services
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+                .AddDataAnnotationsLocalization();
+            */
 
 
             services.Configure<RequestLocalizationOptions>(options => {
